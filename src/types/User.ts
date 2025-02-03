ad266ea4ab4589356d57daababc2a3ca064d344b
Shoshana Gross <shoshana@lightricks.com>
2025-02-03 09:59:52 +0200

    Users: Add interfaces to represent users.

---
+++ src/types/User.ts
@@ -0,0 +1,46 @@
+interface Address {
+  address: string;
+  city: string;
+  state: string;
+  country: string;
+}
+
+interface Company {
+  department: string;
+  name: string;
+  title: string;
+  address: Address;
+}
+
+interface PostComment {
+  body: string;
+  userId: number;
+}
+
+interface Post {
+  id: number;
+  image: string;
+  comments: PostComment[];
+}
+
+interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  maidenName?: string;
+  age: number;
+  gender: string;
+  email: string;
+  phone: string;
+  username: string;
+  password: string;
+  birthDate: string;
+  image: string;
+  address: Address;
+  university?: string;
+  company?: Company;
+  role?: string;
+  posts: Post[];
+}
+
+export type { User, Address, Company, Post, PostComment };
